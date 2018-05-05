8dc10098aa4b9cfd237cf4e8bf2e22a71058ead9
Alireza Andersen <alireza.andersen@gmail.com>
2018-05-04 20:54:10 -0600

    updated const for this.props

--- src/components/input.js
+++ src/components/input.js
@@ -4,14 +4,27 @@ import { Input as MaterializeInput, Row } from 'react-materialize'
 
 export default class Input extends Component {
   render() {
-    const { placeholder } = this.props
+    const { placeholder, handleInputChange, name } = this.props
     return (
-      <MaterializeInput m={12} type='text' placeholder={placeholder}>
+      <MaterializeInput
+        m={12}
+        type='text'
+        name={name}
+        placeholder={placeholder}
+        onChange={handleInputChange}
+      >
       </MaterializeInput>
     )
   }
 }
 
+Input.defaultProps = {
+  placeholder: 'Test',
+  handleInputChange: () => {}
+}
+
 Input.propTypes = {
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  handleInputChange: PropTypes.func.isRequired
 }
